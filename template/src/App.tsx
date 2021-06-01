517a39320ba317fce9ae0b39ad1db89385e91420
Pah Ferreira <pah.webdev@gmail.com>
2021-05-31 22:18:20 -0300

    feat: :sparkles: Styled Theme Provider

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,18 +1,22 @@
 // React
-import React from 'react'
+import React, { FC } from 'react'
 
 // Libraries
 import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
 
 // Custom
 import './App.css'
 import Routes from './routes'
 import store from 'lib/redux'
+import theme from 'lib/styled-components/theme'
 
-const App = () => {
+const App: FC = () => {
   return (
     <Provider store={store}>
-      <Routes />
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
     </Provider>
   )
 }
