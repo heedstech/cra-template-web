cb33233a728b22855d4e8e127c377e909895dac6
Pah Ferreira <pah.webdev@gmail.com>
2021-07-14 20:38:25 -0300

    fix: :bug: Saga Example

--- template/src/storage/example/saga/index.ts
+++ template/src/storage/example/saga/index.ts
@@ -1,24 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// Models
-import { Example } from '../models'
-
 // Libraries
-import * as matchers from 'redux-saga-test-plan/matchers'
-import { testSaga, expectSaga } from 'redux-saga-test-plan'
-import { throwError } from 'redux-saga-test-plan/providers'
+import { SagaIterator } from '@redux-saga/types'
+import { all } from 'redux-saga/effects'
 
-// Custom
-import reducer, { Types } from '../duck'
-
-describe('Example Sagas', () => {
-  describe('Unit Tests', () => {
-    xit('', () => {
-      // Test
-    })
-  })
-  describe('With Reducer', () => {
-    xit('', () => {
-      // Test
-    })
-  })
-})
+export default function* exampleSaga(): SagaIterator {
+  yield all([])
+}
